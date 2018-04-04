a01e17104263a4c0a99c5eef47c364199105e5ec
rpyoungdale <youngdale95@gmail.com>
2018-04-04 08:43:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,37 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+
+      erb :show
+    end
+
+    # post '/student' do
+    #   @student = Student.new(params[:student])
+    #
+    #   params[:student][:courses].each do |details|
+    #     Course.new(details)
+    #   end
+    #
+    #   @courses = Course.all
+    #
+    #   erb :student
+    # end
 
   end
 end
