e480db5ea5e0c92a897effed1e68366f4f8fee1d
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-04 14:16:33 -0400

    lab completed

--- app.rb
+++ app.rb
@@ -4,6 +4,24 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      # {
+      #   "pirate"=>{"name"=>"Bruno", "weight"=>"145", "height"=>"5'10\"",
+      #     "ships"=>[{"name"=>"Taqwa", "type"=>"good", "booty"=>"none"},
+      #       {"name"=>"quba", "type"=>"good", "booty"=>"none"}
+      #      ]
+      #   }
+      # }
+      @pirate = params["pirate"]
+      erb :show
+    end
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
+# The ship class should have name, type, and booty attributes,
+# as well as a class method .all that returns all the ships
+# and a class method .clear that deletes all ships.
+
 class Ship
+  attr_reader :name, :type, :booty
+
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    self.class.all << self
+  end
+  def self.all
+    @all ||= []
+  end
+
+  def self.clear
+    @all = []
+  end
 end
